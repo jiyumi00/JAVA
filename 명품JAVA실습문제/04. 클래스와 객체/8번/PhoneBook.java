e9b537892d0d4d2f27f3ac7710ca8f48fddf5c93
jiyumi <jiyumi00@naver.com>
2024-01-04 22:23:19 +0900

    2개의 클래스 만들기, 객체배열 다루기

--- 명품JAVA실습문제/04. 클래스와 객체/8번/PhoneBook.java
+++ 명품JAVA실습문제/04. 클래스와 객체/8번/PhoneBook.java
@@ -1,16 +1,17 @@
 import java.util.Scanner;
 public class PhoneBook{
-	public static void main(String[]args){
-		Scanner input=new Scanner(System.in);
-		int num;
-		String name,tel,searchName;
-		boolean find=false;
-		
+	Scanner input=new Scanner(System.in);
+	int num;
+	String name,tel,searchName;
+	boolean find=false;
+	Phone []phone;
+	
+	public void inputNum(){
 		System.out.print("인원수>>");
 		num=input.nextInt();
-		
-		Phone []phone=new Phone[num];
-		
+		phone=new Phone[num];
+	}
+	public void saveInfo(){
 		for(int i=0;i<num;i++){
 			System.out.print("이름과 전화번호(이름과 번호는 빈 칸없이 입력)>>");
 			name=input.next();
@@ -18,7 +19,8 @@ public class PhoneBook{
 			phone[i]=new Phone(name,tel);
 		}
 		System.out.println("저장되었습니다...");
-		
+	}
+	public void searchTel(){
 		while(true){
 			find=false;
 			System.out.print("검색할 이름>>");
@@ -34,5 +36,11 @@ public class PhoneBook{
 			}
 			if(find==false) System.out.println(searchName+" 이 없습니다");
 		}
+	}
+	public static void main(String[]args){
+		PhoneBook phonebook=new PhoneBook();
+		phonebook.inputNum();
+		phonebook.saveInfo();
+		phonebook.searchTel();
 	}
 }
