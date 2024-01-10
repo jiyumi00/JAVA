1f965a28c68d7475eee5cf7dfe01c3e44d7135a0
jiyumi <jiyumi00@naver.com>
2024-01-10 11:39:19 +0900

    추상 클래스를 상속받아 서브 클래스 만들기

---
+++ 명품JAVA실습문제/05. 상속/4번/Km2Mile.java
@@ -0,0 +1,14 @@
+class Km2Mile extends Converter{
+	public Km2Mile(double ratio){
+		this.ratio=ratio;
+	}
+	protected double convert(double src){
+		return src/ratio;
+	}
+	protected String getSrcString(){
+		return "Km";
+	}
+	protected String getDestString(){
+		return "mile";
+	}
+}
---
+++ 명품JAVA실습문제/05. 상속/4번/Main.java
@@ -0,0 +1,6 @@
+public class Main{
+	public static void main(String[]args){
+        Km2Mile toMile=new Km2Mile(1.6);
+		toMile.run();
+    }
+}
