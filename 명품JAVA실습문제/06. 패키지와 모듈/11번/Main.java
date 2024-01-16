468a678ceee9f68b8e2985ccf394f59724a3850d
jiyumi <jiyumi00@naver.com>
2024-01-16 09:32:17 +0900

    StringBuffer 클래스 활용

--- 명품JAVA실습문제/06. 패키지와 모듈/11번/Main.java
+++ 명품JAVA실습문제/06. 패키지와 모듈/11번/Main.java
@@ -1,24 +1,44 @@
-import java.util.Scanner;
+import java.util.*;
 public class Main{
-	public static void main(String[]args){
-		Main main=new Main();
-		Scanner scanner=new Scanner(System.in);
+	Scanner scanner=new Scanner(System.in);
+	String str,input;
+	int index;
+	StringBuffer sb;
 	
-		String str;
-		String input; 
+	public void interpret(String input){
+		String []tokens=input.split("!");
 		
+		if(tokens.length!=2 || tokens[0].length()==0 || tokens[1].length()==0){
+			System.out.println("잘못된 명령입니다");
+		}
+		else{
+			index=sb.indexOf(tokens[0]);
+			if(sb.indexOf(tokens[0])==-1){
+				System.out.println("찾을 수 없습니다");
+			}
+			else{
+				sb.replace(index,index+tokens[0].length(),tokens[1]);
+				System.out.println(sb.toString());
+			}
+		}
+		
+	}
+	public void run(){
 		System.out.print(">>");
-		str=scanner.nextLint();
+		str=scanner.nextLine();
+		sb=new StringBuffer(str);
 		
 		while(true){
 			System.out.print("명령: ");
 			input=scanner.next();
-		
-			if(input.equals("그만"){
+			if(input.equals("그만")){
 				break;
 			}
-			
+			interpret(input);
 		}
-		
+	}
+	public static void main(String[]args){
+		Main main=new Main();
+		main.run();
 	}
 }
